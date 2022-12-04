7eac0e0e41489ea7ea07561c359c01a1318e2c6c
turtort74 <dinikkaevv@mail.ru>
2022-12-04 12:49:45 +0300

    Добавьте файлы проекта.

---
+++ Data/Pixel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace MyPhotoshop
+{
+    public struct Pixel
+    {
+        double r;
+        double g;
+        double b;
+
+        public Pixel(double r, double g, double b)
+        {
+            this.r = this.g = this.b = 0;
+            R = r;
+            G = g;
+            B = b;
+        }
+        public double Chek(double value)
+        {
+            if (value < 0 || value > 1)
+                throw new ArgumentOutOfRangeException("value");
+            return value;
+        }
+        public double R
+        {
+            get { return r;}
+            set { r = Chek(value); }
+        }
+
+        public double G
+        {
+            get { return g;}
+            set { g = Chek(value); }
+        }
+
+        public double B
+        {
+            get { return b; }
+            set { b = Chek(value); }
+        }
+
+        public static Pixel operator *(Pixel p, double c)
+        {
+            return new Pixel(Trim(p.R * c), Trim(p.G * c), Trim(p.B * c));
+        }
+
+        public static Pixel operator *(double c, Pixel p)
+        {
+            return p * c;
+        }
+        public static double Trim(double value) => (value < 0) ? 0 : (value > 1) ? 1 : value;
+    }
+}
---
+++ Filters/LighteningFilter.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace MyPhotoshop
+{
+	public class LighteningFilter : IFilter
+	{
+		public ParameterInfo[] GetParameters()
+		{
+			return new []
+			{
+				new ParameterInfo { Name="Коэффициент", MaxValue=10, MinValue=0, Increment=0.1, DefaultValue=1 }
+				
+			};
+		}
+		
+		public override string ToString ()
+		{
+			return "Осветление/затемнение";
+		}
+		
+		public Photo Process(Photo original, double[] parameters)
+		{
+			var result=new Photo(original.width, original.height);
+            for (int x = 0; x < result.width; x++)
+            {
+                for (int y = 0; y < result.height; y++)
+                {
+                    result[x, y] = original[x, y] * parameters[0];
+                }
+            }
+            return result;
+		}
+	}
+}
+
